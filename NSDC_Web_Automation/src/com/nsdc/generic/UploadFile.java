4509234f4c81b53b986d63b050ab977395e20a84
sheetal <sheetal.s@transneuron.com>
2018-06-13 13:16:30 +0530

    Added functionality for Windows upload.

--- NSDC_Web_Automation/src/com/nsdc/generic/UploadFile.java
+++ NSDC_Web_Automation/src/com/nsdc/generic/UploadFile.java
@@ -12,28 +12,39 @@ public class UploadFile
     
     public static void upload(String filePath) throws Exception
     {
+    	File file = new File(filePath);
+        
+        StringSelection stringSelection= new StringSelection(file.getAbsolutePath());
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+        
+        Robot robot = new Robot();
+             
         String osType = ReadWriteData.getData("./TestData/TestData-Config.xls", "Config", 1, 2);
         
         if (osType.equals("Windows"))
         {
+        	 //Paste the clipboard value
+        	
+        	robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_V);
+            
+            //Press Enter key to close the Goto window and Upload window
+            
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
             
         }
         
         else if (osType.equals("Mac OS"))
         {
-            File file = new File(filePath);
-            
-            StringSelection stringSelection= new StringSelection(file.getAbsolutePath());
-            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-            
-            Robot robot = new Robot();
-            
-            //Perform click in middle screen
+        	//Perform click in middle screen
             
             robot.mouseMove(635, 285);
             robot.mousePress(InputEvent.BUTTON1_MASK);
             robot.mouseRelease(InputEvent.BUTTON1_MASK);
-            
+       
             //Open Goto window
             
             robot.keyPress(KeyEvent.VK_META);
