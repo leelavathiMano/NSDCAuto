8297f7415e1651db9623b6e105ee5cc12eff6ffc
Shashank Jha <shashank.j@transneuron.com>
2018-07-09 19:12:09 +0530

    Url changes implemented and database configuration.

--- NSDC_Web_Automation/src/com/nsdc/testConfig/DatabaseConnection.java
+++ NSDC_Web_Automation/src/com/nsdc/testConfig/DatabaseConnection.java
@@ -1,13 +1,9 @@
 package com.nsdc.testConfig;
 
-import java.util.Arrays;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import com.mongodb.Mongo;
 
 public class DatabaseConnection
 {
@@ -17,9 +13,12 @@ public class DatabaseConnection
     {
         try
         {      	
-        	MongoCredential credential = MongoCredential.createCredential("SDMSTestUser", "ekaushalnsdc", "Sd04Ms#10^Te5#sT".toCharArray());
+        	/*MongoCredential credential = MongoCredential.createCredential("SDMSTestUser", "ekaushalnsdc", "Sd04Ms#10^Te5#sT".toCharArray());
         	@SuppressWarnings({ "deprecation", "resource" })
-			MongoClient mongo = new MongoClient(new ServerAddress("13.232.9.250", 27017), Arrays.asList(credential));
+			MongoClient mongo = new MongoClient(new ServerAddress("13.126.28.180", 27017), Arrays.asList(credential));*/
+        	
+        	@SuppressWarnings("deprecation")
+			Mongo mongo = new Mongo("13.126.28.180", 27017);
         	
             @SuppressWarnings("deprecation")
             DB db = mongo.getDB("ekaushalnsdc");
