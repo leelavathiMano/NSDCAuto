2d59072b0bf6a4e68a5a2a5ff985732a91e35cbf
KirtiSingh <kirti.s@transneuron.com>
2018-07-25 11:49:00 +0530

    TP SPOC Workflow

--- NSDC_Web_Automation/src/com/nsdc/regressionTest/TPSPOCChangeWorkflowTestSC_10.java
+++ NSDC_Web_Automation/src/com/nsdc/regressionTest/TPSPOCChangeWorkflowTestSC_10.java
@@ -36,17 +36,22 @@ public class TPSPOCChangeWorkflowTestSC_10 extends TestConfiguration
 		tpsp.enterTP_SPOCFullName(spocName);
 		tpsp.enterTP_SPOCEmailAddress(spocEmail);
 		tpsp.enterTP_SPOCMobileNumber(spocMobileNumber);
+		Thread.sleep(2000);
 		tpsp.clickOnSaveAndNext();
 		Thread.sleep(2000);
 		tpsp.enterTPIDTextbox(tpUsername);
 		tpsp.enterPassword(tpPassword);
+		Thread.sleep(2000);
 		tpsp.clickOnVerify();
 		tpsp.enterMobileOTP(mobileOTP);
 		tpsp.enterEmailOTPTextbox(emailOTP);
+		Thread.sleep(2000);
 		tpsp.clickOnVerify();
+		Thread.sleep(2000);
 		tpsp.clickOnOk();
 		
 		PostLoginPage plp = new PostLoginPage(driver);
+		Thread.sleep(2000);
 		plp.clickOnProfileLogo();
 		plp.clickOnLogout();
 	}
@@ -75,14 +80,17 @@ public class TPSPOCChangeWorkflowTestSC_10 extends TestConfiguration
 		String email = driver.findElement(By.xpath("//input[@formcontrolname='email']")).getAttribute("value");
 		tps.enterTP_SPOCMobileNumber(oldmobileNumber);
 		String mobileNumber = driver.findElement(By.xpath("//input[@formcontrolname='mobileNumber']")).getAttribute("value");
+		Thread.sleep(2000);
 		tps.clickOnSaveAndNext();
 		Thread.sleep(2000);
 		tps.enterTPIDTextbox(tPUsername);
 		tps.enterPassword(tPPassword);
+		Thread.sleep(2000);
 		tps.clickOnVerify();
 		Thread.sleep(2000);
 		tps.enterMobileOTP(spocMobileOTP);
 		tps.enterEmailOTPTextbox(spocEmailOTP);
+		Thread.sleep(2000);
 		tps.clickOnVerify();
 		Thread.sleep(2000);
 
@@ -93,14 +101,17 @@ public class TPSPOCChangeWorkflowTestSC_10 extends TestConfiguration
 			tps.clickOnCancel();
 			tps.enterTP_SPOCEmailAddress(newSPOCEmail);
 			tps.enterTP_SPOCMobileNumber(newSPOCMobileNumber);
+			Thread.sleep(2000);
 			tps.clickOnSaveAndNext();
 			Thread.sleep(2000);
 			tps.enterTPIDTextbox(tPUsername);
 			tps.enterPassword(tPPassword);
+			Thread.sleep(2000);
 			tps.clickOnVerify();
 			Thread.sleep(5000);
 			tps.enterMobileOTP(spocMobileOTP);
 			tps.enterEmailOTPTextbox(spocEmailOTP);
+			Thread.sleep(2000);
 			tps.clickOnVerify();
 			Thread.sleep(2000);
 			tps.clickOnOk();
@@ -111,13 +122,16 @@ public class TPSPOCChangeWorkflowTestSC_10 extends TestConfiguration
 		{
 			tps.enterTP_SPOCEmailAddress(newSPOCEmail);
 			tps.enterTP_SPOCMobileNumber(newSPOCMobileNumber);
+			Thread.sleep(2000);
 			tps.clickOnSaveAndNext();
 			Thread.sleep(2000);
 			tps.enterTPIDTextbox(tPUsername);
 			tps.enterPassword(tPPassword);
+			Thread.sleep(2000);
 			tps.clickOnVerify();
 			tps.enterMobileOTP(spocMobileOTP);
 			tps.enterEmailOTPTextbox(spocEmailOTP);
+			Thread.sleep(2000);
 			tps.clickOnVerify();
 			tps.clickOnOk();
 		}
