5dd2877a0a3f2ef8be5482a3263eed0c767ea56d
Pallavi TR <pallavi.tr@transneuron.com>
2019-03-25 12:46:44 +0530

    no message

--- NSDC_Web_Automation/src/com/nsdc/regressionTest/ToT_BatchApplicantsWorkflowTestSC_17.java
+++ NSDC_Web_Automation/src/com/nsdc/regressionTest/ToT_BatchApplicantsWorkflowTestSC_17.java
@@ -2205,6 +2205,7 @@ public class ToT_BatchApplicantsWorkflowTestSC_17 extends TestConfiguration
         	UploadFile.upload(domainJobRoleAttendance);
         	tVp.clickToUploadChoosedAttendanceSupportFile();
         	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'uploaded successfully')]")));
+        	wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[contains(text(),'uploaded successfully')]")));
         	Assert.assertTrue(driver.findElement(By.xpath("//tr[td[contains(text(),'"+domainJobRoleAttendance+"')]]")).isDisplayed());
         	tVp.clickToDeleteUploadedAttendanceSupportDoc();
         	Assert.assertTrue(driver.findElements(By.xpath("//tr[td[contains(text(),'"+domainJobRoleAttendance+"')]]")).size()==0);
