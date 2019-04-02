ff26418df3586fc4b6b41d7a9e9e983588bae206
Pallavi TR <pallavi.tr@transneuron.com>
2019-04-02 15:29:42 +0530

    no message

--- NSDC_Web_Automation/src/com/nsdc/regressionTest/NSDC_RMSPOC_RozgarMelaWorkflowTestSC_16.java
+++ NSDC_Web_Automation/src/com/nsdc/regressionTest/NSDC_RMSPOC_RozgarMelaWorkflowTestSC_16.java
@@ -104,6 +104,7 @@ public class NSDC_RMSPOC_RozgarMelaWorkflowTestSC_16 extends TestConfiguration
 		rmCp.clickSaveAndAlignSSCs();
 		wait.until(ExpectedConditions.urlContains("align-ssc"));
 		Assert.assertTrue(driver.getCurrentUrl().contains("align-ssc"),"OMG!!! Navigation to Align SSCs page is unsuccessful OR Something wrong! ");
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("blockUI.blockOverlay")));
 		rmCp.clickToSelectAllSSCs();
 		rmCp.clickToAlignSelectedSSCs();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("swal2-title")));
