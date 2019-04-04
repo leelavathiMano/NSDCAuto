c4649bdc55a31b92e8dbb59efad8350d75c8aa75
Pallavi TR <pallavi.tr@transneuron.com>
2019-04-04 15:00:36 +0530

    no message

--- NSDC_Web_Automation/src/com/nsdc/regressionTest/SSC_ToT_ToA_ToMT_ToMA_BatchWorkflowTestSC_11.java
+++ NSDC_Web_Automation/src/com/nsdc/regressionTest/SSC_ToT_ToA_ToMT_ToMA_BatchWorkflowTestSC_11.java
@@ -102,11 +102,11 @@ public class SSC_ToT_ToA_ToMT_ToMA_BatchWorkflowTestSC_11 extends TestConfigurat
 			sscTbcP.selectBatchType(batchType);
 			sscTbcP.selectBatchCategory(batchCategory);
 			sscTbcP.selectTrainingStartDateForBatch(configBatchStartDate);
-			if(batchType.equalsIgnoreCase("Training of Trainer-New")||batchType.equalsIgnoreCase("Disability Sensitization Training for Trainers - NEW"))
+			if(batchType.equalsIgnoreCase("Training of Trainer-New")||batchType.equalsIgnoreCase("Training of Assessor-New")||batchType.equalsIgnoreCase("Disability Sensitization Training for Trainers - NEW")||batchType.equalsIgnoreCase("Disability Sensitization Training for Assessors - NEW"))
 			{
 				sscTbcP.selectTrainingEndDateForNewBatch(configBatchEndDate);
 			}
-			else if(batchType.equalsIgnoreCase("Training of Master Trainer")||batchType.equalsIgnoreCase("Training of Trainer-Existing")||batchType.equalsIgnoreCase("Disability Sensitization Training for Trainers - EXISTING")||batchType.equalsIgnoreCase("Disability Sensitization Training for Master Trainers"))
+			else if(batchType.equalsIgnoreCase("Training of Trainer-Existing")||batchType.equalsIgnoreCase("Training of Master Trainer")||batchType.equalsIgnoreCase("Training of Master Assessor")||batchType.equalsIgnoreCase("Training of Assessor-Existing")||batchType.equalsIgnoreCase("Disability Sensitization Training for Trainers - EXISTING")||batchType.equalsIgnoreCase("Disability Sensitization Training for Assessors - EXISTING")||batchType.equalsIgnoreCase("Disability Sensitization Training for Master Trainers")||batchType.equalsIgnoreCase("Disability Sensitization Training for Master Assessors"))
 			{
 				sscTbcP.selectTrainingEndDateForExistingBatch(configBatchEndDate);
 			}
@@ -1743,9 +1743,40 @@ public class SSC_ToT_ToA_ToMT_ToMA_BatchWorkflowTestSC_11 extends TestConfigurat
 		String trainingEndDate=driver.findElement(By.xpath("//input[@formcontrolname='endDate']")).getAttribute("value").replaceAll("/", "-");
 		ReadWriteData.setExcelData("./TestData/Workflow/SSC-ToT-ToA-ToMT-ToMA-Batch-Workflow.xls", "BatchRejection",Integer.parseInt(serialNum) , 10, trainingEndDate);
 		sscTbcP.selectDomainJobRole(domainJobRole);
-		Thread.sleep(2000);
-		sscTbcP.selectPlatformJobRole(platformJobRole);
-		Thread.sleep(2000);
+		//Sometimes platform job role will not be available
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//angular2-multiselect[@formcontrolname='platformJobRole']/div")));
+		driver.findElement(By.xpath("//angular2-multiselect[@formcontrolname='platformJobRole']/div")).click();
+		if(driver.findElements(By.xpath("//label[text()='"+platformJobRole+"']")).size()==0)
+		{
+			js.executeScript("window.scrollBy(0,-1000)", "");
+			driver.findElement(By.linkText("ToT, ToA, ToMT,ToMA")).click();
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[contains(text(),'Please wait...')]")));
+			js.executeScript("arguments[0].click();",driver.findElement(By.xpath("//span[contains(text(),'Create new Batch for ToT, ToA, ToMT, ToMA')]")));
+			wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[contains(text(),'Please wait...')]")));
+			sscTbcP.selectSubSector(subSector);
+			sscTbcP.selectBatchType(batchType);
+			sscTbcP.selectBatchCategory(batchCategory);
+			sscTbcP.selectTrainingStartDateForBatch(configBatchStartDate);
+			if(batchType.equalsIgnoreCase("Training of Trainer-New")||batchType.equalsIgnoreCase("Training of Assessor-New")||batchType.equalsIgnoreCase("Disability Sensitization Training for Trainers - NEW")||batchType.equalsIgnoreCase("Disability Sensitization Training for Assessors - NEW"))
+			{
+				sscTbcP.selectTrainingEndDateForNewBatch(configBatchEndDate);
+			}
+			else if(batchType.equalsIgnoreCase("Training of Trainer-Existing")||batchType.equalsIgnoreCase("Training of Master Trainer")||batchType.equalsIgnoreCase("Training of Master Assessor")||batchType.equalsIgnoreCase("Training of Assessor-Existing")||batchType.equalsIgnoreCase("Disability Sensitization Training for Trainers - EXISTING")||batchType.equalsIgnoreCase("Disability Sensitization Training for Assessors - EXISTING")||batchType.equalsIgnoreCase("Disability Sensitization Training for Master Trainers")||batchType.equalsIgnoreCase("Disability Sensitization Training for Master Assessors"))
+			{
+				sscTbcP.selectTrainingEndDateForExistingBatch(configBatchEndDate);
+			}
+			trainingStartDate=driver.findElement(By.xpath("//input[@formcontrolname='startDate']")).getAttribute("value").replaceAll("/", "-");
+			ReadWriteData.setExcelData("./TestData/Workflow/SSC-ToT-ToA-ToMT-ToMA-Batch-Workflow.xls", "BatchRejection",Integer.parseInt(serialNum) , 9, trainingStartDate);
+			trainingEndDate=driver.findElement(By.xpath("//input[@formcontrolname='endDate']")).getAttribute("value").replaceAll("/", "-");
+			ReadWriteData.setExcelData("./TestData/Workflow/SSC-ToT-ToA-ToMT-ToMA-Batch-Workflow.xls", "BatchRejection",Integer.parseInt(serialNum) , 10, trainingEndDate);
+			sscTbcP.selectDomainJobRole(domainJobRole);
+			driver.findElement(By.xpath("//angular2-multiselect[@formcontrolname='platformJobRole']/div")).click();
+			sscTbcP.selectPlatformJobRole(platformJobRole);
+		}
+		else
+		{
+			sscTbcP.selectPlatformJobRole(platformJobRole);
+		}
 		//Domain QP
 		sscTbcP.clickToChooseDomainQP();
 		Thread.sleep(4000);
