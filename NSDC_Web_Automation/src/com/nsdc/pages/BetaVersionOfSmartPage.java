69d4eae550c9c2d97a84c9456fb5aa40665c499a
pallavitr <pallavi.tr@transneuron.com>
2019-07-17 16:25:27 +0530

    updated tc-my candidtae and beta version page elements

--- NSDC_Web_Automation/src/com/nsdc/pages/BetaVersionOfSmartPage.java
+++ NSDC_Web_Automation/src/com/nsdc/pages/BetaVersionOfSmartPage.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BetaVersionOfSmartPage 
 {
 	WebDriver driver;
-	@FindBy(xpath="(//button[text()='Close'])[1]")
+	@FindBy(css="button.close")
 	private WebElement closeButton;
 	@FindBy(xpath="(//button[text()='Close'])[2]")
 	private WebElement close_SecondButton;
@@ -21,6 +23,8 @@ public class BetaVersionOfSmartPage
 	}
 	public void clickToClose()
 	{
+		WebDriverWait wait=new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOf(closeButton));
 		closeButton.click();
 	}
 	
