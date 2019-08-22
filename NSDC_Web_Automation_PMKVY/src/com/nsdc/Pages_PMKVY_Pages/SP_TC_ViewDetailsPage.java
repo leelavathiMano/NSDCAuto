96ecc3b0b9c57cb132fd5486e9bb536b3c039108
subhashiniu <subhashini.u@transneuron.com>
2019-08-22 15:53:39 -0700

    create pages and updating the code

---
+++ NSDC_Web_Automation_PMKVY/src/com/nsdc/Pages_PMKVY_Pages/SP_TC_ViewDetailsPage.java
@@ -0,0 +1,45 @@
+package com.nsdc.Pages_PMKVY_Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SP_TC_ViewDetailsPage {
+	WebDriver driver;
+	@FindBy(xpath="(//a[i[@class='la la-ellipsis-h']])[1]")
+	private WebElement SP_ActionButton ;
+	@FindBy(xpath="(//a[contains(text(),'View Details')])[1]")
+	private WebElement ViewDetailsButton1 ;
+	@FindBy(xpath="//div[div[p[text()='All My Batches']]]")
+	private WebElement ViewDetailsButton ;
+	@FindBy(xpath="//div[div[p[text()='All My Batches']]]")
+	private WebElement  View_All_BatchesIcon;
+	@FindBy(xpath="//button[contains(text(),'Create Batch')] ")
+	private WebElement CreateBatchaButton;
+	public SP_TC_ViewDetailsPage(WebDriver driver) 
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void ClickOnSPAction() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		SP_ActionButton.click();
+	}
+	public void CLickOnViewDetails()
+	{
+		ViewDetailsButton1.click();
+	}
+	public void ClickOnView_All_Batches() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		View_All_BatchesIcon.click();
+	}
+	 public void ClickOnCreateBatch() throws InterruptedException
+	 {
+		 Thread.sleep(2000);
+		 CreateBatchaButton.click();
+	 }
+}
--- NSDC_Web_Automation_PMKVY/src/com/nsdc/RegressionTest_PMKVY_STT_Test/PMKVY_SP_workflowTest.java
+++ NSDC_Web_Automation_PMKVY/src/com/nsdc/RegressionTest_PMKVY_STT_Test/PMKVY_SP_workflowTest.java
@@ -10,6 +10,9 @@ import com.nsdc.Pages_PMKVY_Pages.PMKVY_SPPMUpage;
 import com.nsdc.Pages_PMKVY_Pages.PMKVY_SP_ApprovalInprogress_Additionaldetails_Page;
 import com.nsdc.Pages_PMKVY_Pages.Pmkvy_SpecialProjectCreationPage;
 import com.nsdc.Pages_PMKVY_Pages.SPPMU_All_TP_ProjectimplementingAgencypage;
+import com.nsdc.Pages_PMKVY_Pages.SP_TC_DashBoardpage;
+import com.nsdc.Pages_PMKVY_Pages.SP_TC_MyApprovedSchemeOrProgramPage;
+import com.nsdc.Pages_PMKVY_Pages.SP_TC_ViewDetailsPage;
 import com.nsdc.Pages_PMKVY_Pages.STTPMU_All_TP_ProjectimplementingAgencyPage;
 import com.nsdc.Pages_PMKVY_Pages.SelectSchemeOrProgramPage;
 import com.nsdc.Pages_PMKVY_Pages.TP_SP_AdditionalRequiredPage;
@@ -39,8 +42,214 @@ public class PMKVY_SP_workflowTest extends TestConfiguration {
 			String Disability,String Religion,String Gender,String Category, String DiscountsonBaseRate,String BatchDuration,String NumberofTranches,
 			String Tranche3tobepaidwithoutPlacement,String DualLogoRequired,String TargetBeneficiaries,String numberofsectors,String Sector,String JobRole,String Target,
 			String locationState,String locationDistrict,String jobrole1,String targetvalue1,String Jobrole2,
-			String targetvalue2, String tcName,String tcTarget,String advanceTrancheYes,String advancepayment,String duallogoImg,String review ,String comments) throws Exception {
+			String targetvalue2, String tcName,String tcTarget,String advanceTrancheYes,String advancepayment,String duallogoImg,String review ,String comments,
+			String TCloginId,String TCpassword) throws Exception {
 
+//		LaunchPage lp = new LaunchPage(driver);
+//		lp.clickLogin();
+//		 Thread.sleep(3000);
+//		BetaVersionOfSmartPage bsp = new BetaVersionOfSmartPage(driver);
+//		bsp.clickToClose();
+//		 Thread.sleep(3000);
+//		EnterLoginPage elp = new EnterLoginPage(driver);
+//		elp.performlogin(username, password);
+//		Thread.sleep(3000);
+//		DashboardPage db = new DashboardPage(driver);
+//		db.ClickMySchemeOrPrograms();
+//		MySchemeOrProjectPage msp=new MySchemeOrProjectPage(driver);
+//		msp.ClickApplySchemeProgram();
+//		SelectSchemeOrProgramPage ssp= new SelectSchemeOrProgramPage(driver);
+//		Thread.sleep(3000);
+//
+//		ssp.ClickPageNumber3();
+//
+//		Thread.sleep(3000);
+//   ssp.ClickOn_SP_Action();
+//   ssp.ClickOn_SP_AddProject();
+//   
+//   Pmkvy_SpecialProjectCreationPage sp=new Pmkvy_SpecialProjectCreationPage(driver);
+//   Thread.sleep(2000);
+//sp.EnterTheProjectProposalID(projectproposalID);
+//sp.EnterTheProjecName(projectname);
+//sp.ClickProjectType();
+//sp.selectRegisteredAslist(registeredAs);
+//sp.EnterProposingOrganisationName(proposingorganisationName);
+//sp.selectState(state);
+//sp.selectDistrict(district);
+//Thread.sleep(3000);
+//sp.clickAdd();
+//sp.clickOnIndemnityBondBrowse();
+//Thread.sleep(3000);
+//UploadFile.upload(img1);
+//Thread.sleep(3000);
+//
+//sp.clickOnIndemnityBondUploadButton();
+//Thread.sleep(3000);
+//
+////sp.clickOnOtherSupportingDocumentsBrowse();
+////Thread.sleep(3000);
+////UploadFile.upload(img2);
+////Thread.sleep(3000);
+//
+//sp.clickOnOtherSupportingDocumentsUpload();
+//Thread.sleep(3000);
+//sp.clickSaveAndSumbit();
+//Thread.sleep(3000);
+//LogOutPage plp = new LogOutPage(driver);
+//		plp.clickOnProfileLogo();
+//		plp.clickOnLogout();
+//		//
+//		Thread.sleep(3000);
+//		lp.clickLogin();
+//		bsp.clickToClose();
+//		elp.performlogin(loginId, password2);
+//		PMKVY_SPPMUpage sppmu=new PMKVY_SPPMUpage(driver);
+//		Thread.sleep(3000);
+//		sppmu.ClickOnViewAllSchemeAddRequests();
+//		
+//		STTPMU_All_TP_ProjectimplementingAgencyPage allPIA=new STTPMU_All_TP_ProjectimplementingAgencyPage(driver);
+//		Thread.sleep(3000);
+//		allPIA.ClickOnAction();
+//		Thread.sleep(3000);
+//		allPIA.ClickOnViewDetails();
+//		PMKVY_SPPMU_SelectedSchemepage sssp=new PMKVY_SPPMU_SelectedSchemepage(driver);
+//		Thread.sleep(3000);
+//		sssp.ClickAggrementDate();
+//	
+//	sssp.SelectAggrementDate(agrementDate);
+//	
+//		sssp.ClickStartDate();
+//	sssp.SelectProjectDurationStartDate(StartDate);
+//	
+//	sssp.ClickEndDate();
+//	
+//		sssp.SelectProjectDurationEndDate(endDate);
+//	Thread.sleep(3000);
+//		sssp.SelectAdharRequirmentYes(Adhar);
+//		sssp.SelectTrainerCertifiedYes(TrainerCertified);
+//		sssp.SelectTypeofTrainingCentre(TypeofTrainingCentre);
+//		sssp.SelectPlacementsRequirementYes(PlacementsRequirement);
+//		sssp.SelectTypeOfEmployment(TypeOfEmployment);
+//		sssp.EnterTheMinimumPercentageofCandidatedtobeplacedTextBox(MinimumPercentageplaced);
+//		sssp.SelectDisability(Disability);
+//		sssp.SelectReligion(Religion);
+//		sssp.SelectGender(Gender);
+//		sssp.SelectCategory(Category);
+//		sssp.EnterTheDiscountsOnBaseRate(DiscountsonBaseRate);
+//		sssp.EnterBatchDuration(BatchDuration);
+//		sssp.SelectNumberOfTranches(NumberofTranches);
+//		sssp.SelecttrancheThreeToBepaid(Tranche3tobepaidwithoutPlacement);
+//		sssp.SelectDualLogoRequired(DualLogoRequired);
+//		sssp.EnterTheTargetBeneficiaries(TargetBeneficiaries);
+//		
+//			
+//			
+//		sssp.SelectSector(Sector);
+//		sssp.SelectjobRoleName(JobRole);
+//		Thread.sleep(3000);
+//		sssp.EnterSectorAndJobTarget(Target);
+//		Thread.sleep(3000);
+//		sssp.ClickOnSectorAndJobRoleAdd();
+//	
+//		
+//		//sssp.SelectlocationState(locationState);
+//		//sssp.Selectlocationdistrict(locationDistrict);
+//		//sssp.ClicklocationAddButton();
+//		sssp.SelectJobRoleTargetDistribution1(jobrole1);
+//		Thread.sleep(3000);
+//		sssp.EnterTargetDistribution1(targetvalue1);
+//		sssp.SelectTCName(tcName);
+//		sssp.EnterTcTarget(tcTarget);
+//		Thread.sleep(3000);
+//		sssp.ClickAddTcTargetButton();
+//		
+//		//sssp.SelectJobRoleTargetDistribution2(Jobrole2);
+//		//sssp.EnterTargetDistribution2(targetvalue2);
+//		sssp.SelectAdvanceTranche(advanceTrancheYes);
+//	
+//		sssp.EnterpercentageOfAdvanceTextBox(advancepayment);
+//		
+//		sssp.ClickAmountTobeOfAdvance();
+//		Thread.sleep(3000);
+//		sssp.ClickAdvanceAdd();
+//		
+//		sssp.ClickSaveAndSubmitButton();
+//		Thread.sleep(3000);
+//		plp.clickOnProfileLogo();
+//		plp.clickOnLogout();
+//		
+//		///////////////////
+//		lp.clickLogin();
+//		bsp.clickToClose();
+//		elp.performlogin(username, password);
+//		db.ClickMySchemeOrPrograms();
+//		ssp.ClickApprovalInProgress_SPAction();
+//		ssp.ClickApprovalInProgress_SP_ViewDetails();
+//		PMKVY_SP_ApprovalInprogress_Additionaldetails_Page  addtionalpage=new PMKVY_SP_ApprovalInprogress_Additionaldetails_Page(driver);	
+//		Thread.sleep(3000);
+//		addtionalpage.ClickOn_Action();
+//		addtionalpage.Click_AdditionalDetailsRequried_Action();
+//		////////addtionalreqDetails
+//		
+//		TP_SP_AdditionalRequiredPage addR=new TP_SP_AdditionalRequiredPage(driver);
+//		Thread.sleep(3000);
+//		addR.ClickOnDual_logoBrowse();
+//		Thread.sleep(3000);
+//		UploadFile.upload(duallogoImg);
+//		Thread.sleep(3000);
+//		addR.ClickOnDual_logoUpLoad();
+//		Thread.sleep(3000);
+//		addR.ClickSigned_TermSheetBrowse();
+//		Thread.sleep(3000);
+//		UploadFile.upload(img1);
+//		Thread.sleep(3000);
+//		addR.clickSigned_TermSheetUpLoad();
+//		Thread.sleep(3000);
+//		addR.SelectReview(review);
+//		Thread.sleep(3000);
+//		addR.ClickOnDownloadTermsheet();
+//		Thread.sleep(3000);
+//		addR.ClickSaveAndSubmit();
+//		
+//		
+//		Thread.sleep(3000);
+//		plp.clickOnProfileLogo();
+//		plp.clickOnLogout();
+//		///
+//		lp.clickLogin();
+//		bsp.clickToClose();
+//		elp.performlogin(loginId, password2);
+//		Thread.sleep(3000);
+//		sppmu.ClickOnViewAllSchemeAddRequests();
+//		//for Sp and STT SAME PAGE ACTION BUTTON AND VIEW DETAILS
+//		SPPMU_All_TP_ProjectimplementingAgencypage iap= new SPPMU_All_TP_ProjectimplementingAgencypage(driver);
+//		Thread.sleep(3000);
+//		iap.ClickOnAction();
+//		iap.ClickOnViewDetails();
+//		///FOR tP AND SPPMU BOTH ADDTIONAL REQ SAME PAGE AND XPATH ALSO SAME(TP_SP_AdditionalRequiredPage)
+//		Thread.sleep(3000);
+//		addR.ClickSigned_TermSheetBrowse();
+//		Thread.sleep(3000);
+//		UploadFile.upload(img1);
+//		Thread.sleep(3000);
+//		addR.clickSigned_TermSheetUpLoad();
+//		Thread.sleep(3000);
+//		addR.SelectComments(comments);
+//		addR.ClickSaveAndSubmit();
+//		//Thread.sleep(3000);
+//		//iap.ClickOnAction();
+//		//Thread.sleep(3000);
+//		//iap.ClickOnViewGeneratedTermSheet();
+//		Thread.sleep(3000);
+//		plp.clickOnProfileLogo();
+//		plp.clickOnLogout();
+		
+		
+		
+		
+		
+		
+		
 		LaunchPage lp = new LaunchPage(driver);
 		lp.clickLogin();
 		 Thread.sleep(3000);
@@ -48,197 +257,23 @@ public class PMKVY_SP_workflowTest extends TestConfiguration {
 		bsp.clickToClose();
 		 Thread.sleep(3000);
 		EnterLoginPage elp = new EnterLoginPage(driver);
-		elp.performlogin(username, password);
-		Thread.sleep(3000);
-		DashboardPage db = new DashboardPage(driver);
-		db.ClickMySchemeOrPrograms();
-		MySchemeOrProjectPage msp=new MySchemeOrProjectPage(driver);
-		msp.ClickApplySchemeProgram();
-		SelectSchemeOrProgramPage ssp= new SelectSchemeOrProgramPage(driver);
-		Thread.sleep(3000);
-
-		ssp.ClickPageNumber3();
-
-		Thread.sleep(3000);
-   ssp.ClickOn_SP_Action();
-   ssp.ClickOn_SP_AddProject();
-   
-   Pmkvy_SpecialProjectCreationPage sp=new Pmkvy_SpecialProjectCreationPage(driver);
-   Thread.sleep(2000);
-sp.EnterTheProjectProposalID(projectproposalID);
-sp.EnterTheProjecName(projectname);
-sp.ClickProjectType();
-sp.selectRegisteredAslist(registeredAs);
-sp.EnterProposingOrganisationName(proposingorganisationName);
-sp.selectState(state);
-sp.selectDistrict(district);
-Thread.sleep(3000);
-sp.clickAdd();
-sp.clickOnIndemnityBondBrowse();
-Thread.sleep(3000);
-UploadFile.upload(img1);
-Thread.sleep(3000);
-
-sp.clickOnIndemnityBondUploadButton();
-Thread.sleep(3000);
-
-//sp.clickOnOtherSupportingDocumentsBrowse();
-//Thread.sleep(3000);
-//UploadFile.upload(img2);
-//Thread.sleep(3000);
-
-sp.clickOnOtherSupportingDocumentsUpload();
-Thread.sleep(3000);
-sp.clickSaveAndSumbit();
-Thread.sleep(3000);
-LogOutPage plp = new LogOutPage(driver);
-		plp.clickOnProfileLogo();
-		plp.clickOnLogout();
-		//
-		Thread.sleep(3000);
-		lp.clickLogin();
-		bsp.clickToClose();
-		elp.performlogin(loginId, password2);
-		PMKVY_SPPMUpage sppmu=new PMKVY_SPPMUpage(driver);
-		Thread.sleep(3000);
-		sppmu.ClickOnViewAllSchemeAddRequests();
+		elp.performlogin(TCloginId, TCpassword);
 		
-		STTPMU_All_TP_ProjectimplementingAgencyPage allPIA=new STTPMU_All_TP_ProjectimplementingAgencyPage(driver);
-		Thread.sleep(3000);
-		allPIA.ClickOnAction();
-		Thread.sleep(3000);
-		allPIA.ClickOnViewDetails();
-		PMKVY_SPPMU_SelectedSchemepage sssp=new PMKVY_SPPMU_SelectedSchemepage(driver);
-		Thread.sleep(3000);
-		sssp.ClickAggrementDate();
-	
-	sssp.SelectAggrementDate(agrementDate);
-	
-		sssp.ClickStartDate();
-	sssp.SelectProjectDurationStartDate(StartDate);
-	
-	sssp.ClickEndDate();
-	
-		sssp.SelectProjectDurationEndDate(endDate);
-	Thread.sleep(3000);
-		sssp.SelectAdharRequirmentYes(Adhar);
-		sssp.SelectTrainerCertifiedYes(TrainerCertified);
-		sssp.SelectTypeofTrainingCentre(TypeofTrainingCentre);
-		sssp.SelectPlacementsRequirementYes(PlacementsRequirement);
-		sssp.SelectTypeOfEmployment(TypeOfEmployment);
-		sssp.EnterTheMinimumPercentageofCandidatedtobeplacedTextBox(MinimumPercentageplaced);
-		sssp.SelectDisability(Disability);
-		sssp.SelectReligion(Religion);
-		sssp.SelectGender(Gender);
-		sssp.SelectCategory(Category);
-		sssp.EnterTheDiscountsOnBaseRate(DiscountsonBaseRate);
-		sssp.EnterBatchDuration(BatchDuration);
-		sssp.SelectNumberOfTranches(NumberofTranches);
-		sssp.SelecttrancheThreeToBepaid(Tranche3tobepaidwithoutPlacement);
-		sssp.SelectDualLogoRequired(DualLogoRequired);
-		sssp.EnterTheTargetBeneficiaries(TargetBeneficiaries);
+		SP_TC_DashBoardpage tcd=new SP_TC_DashBoardpage(driver);
 		
-			
-			
-		sssp.SelectSector(Sector);
-		sssp.SelectjobRoleName(JobRole);
-		Thread.sleep(3000);
-		sssp.EnterSectorAndJobTarget(Target);
-		Thread.sleep(3000);
-		sssp.ClickOnSectorAndJobRoleAdd();
-	
+		tcd.ClickOnMyScheme();
+		SP_TC_MyApprovedSchemeOrProgramPage spApp=new SP_TC_MyApprovedSchemeOrProgramPage(driver);
+		 
+		spApp.ClickOnSPAction();
+		spApp.CLickOnViewDetails();
 		
-		//sssp.SelectlocationState(locationState);
-		//sssp.Selectlocationdistrict(locationDistrict);
-		//sssp.ClicklocationAddButton();
-		sssp.SelectJobRoleTargetDistribution1(jobrole1);
-		Thread.sleep(3000);
-		sssp.EnterTargetDistribution1(targetvalue1);
-		sssp.SelectTCName(tcName);
-		sssp.EnterTcTarget(tcTarget);
-		Thread.sleep(3000);
-		sssp.ClickAddTcTargetButton();
+		SP_TC_ViewDetailsPage pst= new SP_TC_ViewDetailsPage(driver);
+		 
+		pst.ClickOnSPAction();
+		pst.CLickOnViewDetails();
+		pst.ClickOnView_All_Batches();
+		pst.ClickOnCreateBatch();
 		
-		//sssp.SelectJobRoleTargetDistribution2(Jobrole2);
-		//sssp.EnterTargetDistribution2(targetvalue2);
-		sssp.SelectAdvanceTranche(advanceTrancheYes);
-	
-		sssp.EnterpercentageOfAdvanceTextBox(advancepayment);
-		
-		sssp.ClickAmountTobeOfAdvance();
-		Thread.sleep(3000);
-		sssp.ClickAdvanceAdd();
-		
-		sssp.ClickSaveAndSubmitButton();
-		Thread.sleep(3000);
-		plp.clickOnProfileLogo();
-		plp.clickOnLogout();
-		
-		///////////////////
-		lp.clickLogin();
-		bsp.clickToClose();
-		elp.performlogin(username, password);
-		db.ClickMySchemeOrPrograms();
-		ssp.ClickApprovalInProgress_SPAction();
-		ssp.ClickApprovalInProgress_SP_ViewDetails();
-		PMKVY_SP_ApprovalInprogress_Additionaldetails_Page  addtionalpage=new PMKVY_SP_ApprovalInprogress_Additionaldetails_Page(driver);	
-		Thread.sleep(3000);
-		addtionalpage.ClickOn_Action();
-		addtionalpage.Click_AdditionalDetailsRequried_Action();
-		////////addtionalreqDetails
-		
-		TP_SP_AdditionalRequiredPage addR=new TP_SP_AdditionalRequiredPage(driver);
-		Thread.sleep(3000);
-		addR.ClickOnDual_logoBrowse();
-		Thread.sleep(3000);
-		UploadFile.upload(duallogoImg);
-		Thread.sleep(3000);
-		addR.ClickOnDual_logoUpLoad();
-		Thread.sleep(3000);
-		addR.ClickSigned_TermSheetBrowse();
-		Thread.sleep(3000);
-		UploadFile.upload(img1);
-		Thread.sleep(3000);
-		addR.clickSigned_TermSheetUpLoad();
-		Thread.sleep(3000);
-		addR.SelectReview(review);
-		Thread.sleep(3000);
-		addR.ClickOnDownloadTermsheet();
-		Thread.sleep(3000);
-		addR.ClickSaveAndSubmit();
-		
-		
-		Thread.sleep(3000);
-		plp.clickOnProfileLogo();
-		plp.clickOnLogout();
-		///
-		lp.clickLogin();
-		bsp.clickToClose();
-		elp.performlogin(loginId, password2);
-		Thread.sleep(3000);
-		sppmu.ClickOnViewAllSchemeAddRequests();
-		//for Sp and STT SAME PAGE ACTION BUTTON AND VIEW DETAILS
-		SPPMU_All_TP_ProjectimplementingAgencypage iap= new SPPMU_All_TP_ProjectimplementingAgencypage(driver);
-		Thread.sleep(3000);
-		iap.ClickOnAction();
-		iap.ClickOnViewDetails();
-		///FOR tP AND SPPMU BOTH ADDTIONAL REQ SAME PAGE AND XPATH ALSO SAME(TP_SP_AdditionalRequiredPage)
-		Thread.sleep(3000);
-		addR.ClickSigned_TermSheetBrowse();
-		Thread.sleep(3000);
-		UploadFile.upload(img1);
-		Thread.sleep(3000);
-		addR.clickSigned_TermSheetUpLoad();
-		Thread.sleep(3000);
-		addR.SelectComments(comments);
-		addR.ClickSaveAndSubmit();
-		//Thread.sleep(3000);
-		//iap.ClickOnAction();
-		//Thread.sleep(3000);
-		//iap.ClickOnViewGeneratedTermSheet();
-		Thread.sleep(3000);
-		plp.clickOnProfileLogo();
-		plp.clickOnLogout();
 	}
 	
 	 
