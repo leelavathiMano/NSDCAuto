1acd1f456e0b25942147fdda5ab70090fcd9ad5d
subhashiniu <subhashini.u@transneuron.com>
2020-01-27 16:45:40 +0530

    updated

--- NSDC_Web_Automation_NON_PMKVY/src/com/nsdc/generic/ToT_ToA_Batch_DatePicker.java
+++ NSDC_Web_Automation_NON_PMKVY/src/com/nsdc/generic/ToT_ToA_Batch_DatePicker.java
@@ -44,4 +44,50 @@ public class ToT_ToA_Batch_DatePicker
 			driver.findElement(By.xpath("//div[text()='"+day+"']")).click();
 		}
 	}
+	
+	///////////////////////////
+	
+	
+	
+	
+	public static void SelectDate(WebDriver driver, String date, WebElement dateTextField, WebElement monthDropdownList) throws InterruptedException
+	{
+		/*
+		 * WebDriverWait wait=new WebDriverWait(driver,60);
+		 * wait.until(ExpectedConditions.elementToBeClickable(dateTextField));
+		 */Thread.sleep(2000);
+		//JavascriptExecutor js=(JavascriptExecutor)driver;
+		//js.executeScript("arguments[0].click();", dateTextField);
+		//Thread.sleep(2000);
+		String parts[]=date.split("-");
+		String day=parts[0];
+		if(day.startsWith("0"))
+		{
+			day=day.replace("0", "");
+		}
+		String month=parts[1];
+		//String year=parts[2];
+		//SelectDropDownList.selectDropDownListByValue(yearDropdownList, year);
+		SelectDropDownList.selectDropDownListByVisibleText(monthDropdownList, month);
+		if(driver.findElements(By.xpath("//div[text()='"+day+"']")).size()>1)
+		{
+			if(driver.findElement(By.xpath("(//div[text()='"+day+"'])[1]")).getAttribute("class").contains("mute"))
+			{
+				driver.findElement(By.xpath("(//div[text()='"+day+"'])[2]")).click();
+			}
+			else
+			{
+				driver.findElement(By.xpath("(//div[text()='"+day+"'])[1]")).click();
+			}
+		}
+		else
+		{
+			driver.findElement(By.xpath("//div[text()='"+day+"']")).click();
+		}
+	}
+	
+	
+	
+	
+	
 }
